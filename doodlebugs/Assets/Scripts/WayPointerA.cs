73739c7b24425267b7d7dd7cb983bf4412a4794d
Rory <rwilson2@ltu.edu>
2021-03-07 16:50:19 -0500

    bare bones
    i hate myself and my inability to break free of procrastination

---
+++ doodlebugs/Assets/Scripts/WayPointerA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointerA : MonoBehaviour
+{
+    public static Transform[] waypts;
+
+    private void Awake()
+    {
+        waypts = new Transform[transform.childCount];
+
+        for (int i = 0; i < waypts.Length; i++)
+        {
+            waypts[i] = transform.GetChild(i);
+        }
+    }
+}
---
+++ doodlebugs/Assets/Scripts/eneMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eneMovement : MonoBehaviour
+{
+    public float speed = 1;
+
+    private Transform target;
+    private int wayptIndex = 0;
+
+    private void Start()
+    {
+        target = WayPointerA.waypts[0];
+    }
+
+    private void Update()
+    {
+        Vector2 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, target.position) <= 0.2)
+        {
+            NextPoint();
+        }
+    }
+
+    void NextPoint()
+    {
+        if(wayptIndex >= WayPointerA.waypts.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wayptIndex++;
+        target = WayPointerA.waypts[wayptIndex];
+    }
+}
