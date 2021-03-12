67a260e07683434bf08f2cd5420a8871877af75b
Rory <rwilson2@ltu.edu>
2021-03-12 11:15:30 -0500

    The Unity Animation system is a necessary evil.
    Everything that matters is now animated!
    
    - Also, reorganized things
    - I'm starting to realize that Unity might hate while loops

---
+++ doodlebugs/Assets/Scripts/tower scripts/Fire.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject tip, bullet;
+    public float rate;
+    float timer;
+    bool timed = true;
+    public bool stat;
+    public float range;
+    public bool good = false;
+    public bool wave = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //wave = WaveSpawner.WaveTime.isWave;
+
+        ////if (wave)
+        ////{
+        //    timed = true;
+        ////}
+        ////else timed = false;
+
+        //while (good && timed)
+        //{
+            timer -= Time.deltaTime;
+            print(timer);
+            if (timer <= 0)
+            {
+                if (good)
+                {
+                    print("shoot");
+                    Shoot();
+                    timer = 1 / rate;
+                }
+            }
+        //}
+    }
+
+    void Shoot()
+    {
+        GameObject clone = Instantiate(bullet, tip.transform.position, tip.transform.rotation);
+        clone.GetComponent<bulletScript>().life = range;
+        print("shot");
+    }
+}
